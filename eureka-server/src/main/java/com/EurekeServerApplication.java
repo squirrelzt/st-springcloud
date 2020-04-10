bb700d296755af15736b8f76cb8d3354fd590ec2
squirrelzt <squirrelzt@126.com>
2020-04-10 22:25:04 +0800

    eureka server bootstrap class configuration

--- eureka-server/src/main/java/com/EurekeServerApplication.java
+++ eureka-server/src/main/java/com/EurekeServerApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
-@EnableEurekaServer
+
 public class EurekeServerApplication {
 
+    @SpringBootApplication
+    @EnableEurekaServer
+    public static class EurekeServerConfiguration {
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(EurekeServerApplication.class, args);
+        SpringApplication.run(EurekeServerConfiguration.class, args);
     }
 }
